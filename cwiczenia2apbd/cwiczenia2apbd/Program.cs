f62eeeda833a7f747fc1bfff95947ec91844d84e
TomaszWiniarski99 <s33704@pjwstk.edu.pl>
2026-03-12 09:13:31 +0100

    Hello changes for task 2

--- cwiczenia2apbd/cwiczenia2apbd/Program.cs
+++ cwiczenia2apbd/cwiczenia2apbd/Program.cs
@@ -1,10 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
-
-
-
-
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, Michael Jordan!");
+Console.WriteLine("Hello, Muhammad Ali!");
